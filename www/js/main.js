997b0c8992261882c857d027fab49491a38cc9c4
Ben Townshend <bentownshend@icloud.com>
2018-03-08 13:43:11 +0000

    init the qr scanner

--- www/js/main.js
+++ www/js/main.js
@@ -9,6 +9,9 @@ $(document).ready(function() {
 
 function onDeviceReady() {
 	console.log("device ready");
+
+	QRScanner.prepare(onDone);
+
 	init();
 }
 
@@ -24,6 +27,28 @@ function init() {
 	if (window.navigator.onLine) onOnline();
 	else onOffline();
 }
+
+function onDone(err, status){
+	if (err) {
+		// here we can handle errors and clean up any loose ends.
+		console.error(err);
+	}
+	if (status.authorized) {
+		// W00t, you have camera access and the scanner is initialized.
+		// QRscanner.show() should feel very fast.
+		// init();
+		QRscanner.show();
+	} else if (status.denied) {
+		// The video preview will remain black, and scanning is disabled. We can
+		// try to ask the user to change their mind, but we'll have to send them
+		// to their device settings with `QRScanner.openSettings()`.
+	} else {
+		// we didn't get permission, but we didn't get permanently denied. (On
+		// Android, a denial isn't permanent unless the user checks the "Don't
+		// ask again" box.) We can ask again at the next relevant opportunity.
+	}
+}
+
 
 function onOnline() {
 	$("body").addClass("online");
